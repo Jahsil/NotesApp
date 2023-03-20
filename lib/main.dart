c7caf7b3d888a0ca80365ee8ae904a24e20c814b
Eyouel Haile <eyuataait@gmail.com>
2023-03-20 22:22:59 +0300

    added email verification view

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:notes/views/login_view.dart';
+import 'package:notes/views/verify_email_view.dart';
 
 
 void main() async {
@@ -17,6 +18,45 @@ void main() async {
     home:  LoginView(),
   ));
 }
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        centerTitle: true,
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              print("This is teh current user ==========");
+              final user =  FirebaseAuth.instance.currentUser;
+              print(user);
+              if(user?.emailVerified ?? false){
+                return Text("Done");
+              }else{
+                return const VerifyEmailView();
+
+              }
+
+
+            default:
+              return Center(child: LoadingAnimationWidget.inkDrop(
+                  color: Colors.red, size: 50));
+          }
+
+        },
+
+      ),
+    );
+  }
+}
+
 
 
 
